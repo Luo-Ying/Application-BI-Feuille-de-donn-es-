e853128524aa65cad9e609b48c00bb65135740c5
Marie Zephir <mz@efidem.com>
2024-02-26 20:49:28 +0100

    Update of the script for the pie chart

--- Script.py
+++ Script.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import sys
 import os
+import matplotlib.pyplot as plotter
+import math
 
 DTYPE_DICT_LOTS = {'lotId': 'Int64', 'tedCanId': 'Int64', 'correctionsNb': 'Int64', 'cancelled': 'Int64', 'awardDate': 'object', 'awardEstimatedPrice': 'Float64',
 'awardPrice': 'Float64', "cpv" : 'Int64', 'numberTenders' : 'Int64', 'onBehalf': 'object' , 'jointProcurement' : 'object' , 'fraAgreement' : 'object',
@@ -14,7 +16,8 @@ def read_csv(input_csv_path):
         case 'Lots.csv':
             df = pd.read_csv(input_csv_path, dtype=DTYPE_DICT_LOTS, sep=',')
             column = read_user_column(df)
-            choose_Diagram(DTYPE_DICT_LOTS[column])
+            diagram = choose_Diagram()
+            draw_Diagram(diagram, df, column)
         case _:
             print("Action par defaut")
 
@@ -25,13 +28,31 @@ def read_user_column(df):
     print("Veuillez selectionner une colonne : ")
     return input()
 
-def choose_Diagram(column_type):
-    match column_type:
-        case 'Int64':
-        case 'object':
-        case 'Float64':
-        case _:
-            print("Action par defaut")
+def choose_Diagram():
+    print ("Les diagrammes disponibles sont : Camembert , Combo chart, Line Chart, Top 5, Boite moustache, Nuage de points, Tableau, Gauge, Tree map ")
+    print("Veuillez choisir un type de diagramme : ")
+    return input()
+
+def draw_Diagram(diagram, df, column):
+    match diagram:
+        case 'Camembert':
+            draw_Pie_Chart(diagram, df, column)
+
+
+# Les graphes qui peuvent être des camemberts sont : correctionsNb, cancelled, onBehalf, jointProcurement, fraAgreement, fraEstimated
+def draw_Pie_Chart(diagram, df, column):
+    newTableCount = df[column].value_counts(dropna=False).reset_index(name='count')
+    pieLabels = newTableCount[column]
+    pieValues = newTableCount['count']
+
+    figureObject, axesObject = plotter.subplots()
+
+    # Draw the pie chart
+    axesObject.pie(pieValues, labels=pieLabels, autopct='%1.2f', startangle=90)
+
+    # Aspect ratio - equal means pie is a circle
+    axesObject.axis('equal')
+    plotter.show()
 
 if __name__ == '__main__':
     input_csv_path = sys.argv[1]
